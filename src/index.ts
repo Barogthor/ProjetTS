6a97fe37e1cba9c4bf64eb32a729a7029154d726
Barogthor <f.thery3103@gmail.com>
2020-03-30 10:20:09 +0200

    compile ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 class Pokemon {
+    private name: string
     constructor(name) {
         this.name = name
     }
 }
 const pokemon = new Pokemon("pika")
-pokemon.parle = () => console.log("pika pika chhuuuu")
 
 
 
 console.log(pokemon)
-pokemon.parle()
